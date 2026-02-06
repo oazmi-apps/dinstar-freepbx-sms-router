f10b5f8025485d5220b3ee48b1a14850251ecc95
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2026-02-06 00:17:32 -0500

    - in `/Dinstarsms.class.php`, fix implementation so that it actually works correctly now (i.e. outbound text messages are intercepted and forwarded).   - defined the `myDialplanHooks` method, which is absolutely necessary for freepbx to discover that our addon needs to attach asterisk dialplan hooks (i.e. call `doDialplanHook`).   - make `Dinstarsms` extend `FreePBX_Helpers`, as I was getting a runtime installation error in freepbx when trying to get it to `implements DialplanHooks`.     the `FreePBX_Helpers` class already implements the `DialplanHooks` interface, so we'll just use that instead.   - replace the `genConfig` method with `updateAllExtensions`, to apply the custom sms-handling to each user/extension-number.     this is because the documentation suggests that `genConfig` should always be paired with an implementation of `writeConfig`.     however, I do not know were the config file is supposed to be written to, and I don't want to write it to some global config file, like `/etc/asterisk/pjsip.endpoint_custom_post.conf`, as that might overwrite an existing custom extension's config file.     thus, it is better to _tell_ freepbx's extension-number db to use our sms-handler for each extension-number, and let freepbx perform the update to the asterisk configs.   - most of these changes were made by looking at how the popular `simontelephonics/smsconnector` addon repo performs these tasks (specifically in `https://github.com/simontelephonics/smsconnector/blob/release/16.0/Smsconnector.class.php`). - in `/deno.json`, define the `build` task to generate an uploadable addon package.   (the uploading can be done in `http://your_freepbx_webui.com/admin/config.php?display=modules&action=upload`) - also, I hate php for being such an atrocious and inconsistent language.   oh, and I don't use arch btw.

--- Dinstarsms.class.php
+++ Dinstarsms.class.php
@@ -3,12 +3,14 @@
 namespace FreePBX\modules;
 
 use FreePBX\BMO;
-use FreePBX\DialplanHooks;
+use FreePBX\PDO;
+use FreePBX_Helpers;
 
 
-class Dinstarsms implements BMO, DialplanHooks
+class Dinstarsms extends FreePBX_Helpers implements BMO
 {
 	public $freepbx = null;
+	protected $database = null;
 	protected $context = 'dinstar-sms-handler';
 
 	public function __construct($freepbx = null)
@@ -17,15 +19,26 @@ class Dinstarsms implements BMO, DialplanHooks
 			throw new \Exception("[Dinstarsms]: not given a FreePBX object.");
 		}
 		$this->freepbx 	= $freepbx;
+		$this->database = $freepbx->Database;
+		$this->updateAllExtensions();
 	}
 
+	/** needed for freepbx to discover that we perform a dialplan hook! */
+	public function myDialplanHooks(): bool
+	{
+		return true;
+	}
+
+	/** this defines the custom message hook to capture outbound sms text messages,
+	 * which will then be forwarded to our http endpoint.
+	 */
 	public function doDialplanHook(&$ext, $engine, $priority): void
 	{
 		$context = $this->context;
 		// $handler_url = 'http://localhost/admin/modules/dinstarsms/api/outbound.php';
 		$handler_url = 'http://10.0.1.36:3000/freepbx';
-
-		$ext->add($context, '_.', '', new \ext_noop('Routing SIP MESSAGE to internal http handler via POST'));
+		// no-op logging operation.
+		$ext->add($context, '_.', '', new \ext_noop('SIP MESSAGE: from ${MESSAGE(from)} to ${MESSAGE(to)}.'));
 		// first we extract variables from the SIP packet.
 		$ext->add($context, '_.', '', new \ext_set('MSG_TO', '${MESSAGE(to)}'));
 		$ext->add($context, '_.', '', new \ext_set('MSG_FROM', '${MESSAGE(from)}'));
@@ -38,28 +51,34 @@ class Dinstarsms implements BMO, DialplanHooks
 		$ext->add($context, '_.', '', new \ext_set('CURLOPT(httphdr)', 'Content-Type: application/json'));
 		# lastly, we execute the POST request via curl (the presence of the second argument makes it into a post-request).
 		$ext->add($context, '_.', '', new \ext_set('CURL_RESULT', '${CURL(' . $handler_url . ',${JSON_PAYLOAD})}'));
+		// we also relay the message to the actual endpoint, should there be one (so that any custom user receives the message too via pjsip's standard behavior).
+		// $ext->add($context, '_.', '', new \ext_messagesend('pjsip:${EXTERN}', '${CALLERID(num)}'));
 		$ext->add($context, '_.', '', new \ext_hangup());
 	}
 
-	// this method will register our sms asterisk-handler context on pjsip configuration for each user/extension.
-	public function genConfig(): void
+	/** update database of users/extensions to use our custom messaging context. */
+	public function updateAllExtensions()
 	{
 		$context = $this->context;
-		// find each pjsip-based user/extension (we don't process chan-based users).
-		$devices = $this->freepbx->Core->getDevices();
-		foreach ($devices as $device) {
-			if ($device['tech'] !== 'pjsip') {
-				continue;
-			}
-			// TODO: we should check if our module is enabled for this particular user/extension (via the gui logic)
-			// for this pjsip user, we first obtain its extension number, attain its settings object,
-			// modify it, and then re-assign the new settings back to the user, based on their extension number.
-			$extension_number = $device['id'];
-			$device_settings = $this->freepbx->Core->getDevice($extension_number);
-			if ($device_settings['message_context']['value'] !== $context) {
-				$device_settings['message_context']['value'] = $context;
-				// the `true` flag is there to prevent a recursive loop.
-				$this->freepbx->Core->addDevice($extension_number, 'pjsip', $device_settings, true);
+		$db = $this->database;
+		$sql_pjsip = 'SELECT id FROM sip WHERE keyword = "sipdriver" AND data = "chan_pjsip"';
+		$devices = $db->query($sql_pjsip)->fetchAll(\PDO::FETCH_COLUMN);
+		// each "device" is actually just an extension number (user).
+		foreach ($devices as $extension) {
+			// update or insert the custom `message_context` setting.
+			$sql_check = 'SELECT * FROM sip WHERE id = ? AND keyword = "message_context"';
+			$check = $db->prepare($sql_check);
+			$check->execute([$extension]);
+			if ($check->rowCount() > 0) {
+				// override existing `message_context` entry for this particular extension number.
+				$sql_update = 'UPDATE sip SET data = ? WHERE id = ? AND keyword = "message_context"';
+				$stmt = $db->prepare($sql_update);
+				$stmt->execute([$context, $extension]);
+			} else {
+				// insert a new `message_context` entry for this particular extension number.
+				$sql_insert = 'INSERT INTO sip (id, keyword, data, flags) VALUES (?, "message_context", ?, 0)';
+				$stmt = $db->prepare($sql_insert);
+				$stmt->execute([$extension, $context]);
 			}
 		}
 	}
